4881a151754b8ff2f47426e78b87d95cad127eb0
Richard Bernisca <taropogi_123@yahoo.com>
2021-06-28 13:42:12 +0800

    change user seeder eloquent

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -15,29 +16,32 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert(
-            [
-                [
-                    'name' => 'Richard',
-                    'email' => 'richard@reader.com',
-                    'password' => Hash::make('richard_1234'),
-                ],
-                [
-                    'name' => 'Klar',
-                    'email' => 'klar@reader.com',
-                    'password' => Hash::make('klar_1234'),
-                ],
-                [
-                    'name' => 'Rena',
-                    'email' => 'rena@reader.com',
-                    'password' => Hash::make('rena_1234'),
-                ],
-                [
-                    'name' => 'Mike',
-                    'email' => 'mike@reader.com',
-                    'password' => Hash::make('mike_1234'),
-                ],
-            ]
-        );
+        $user1 = new User;
+
+        $user1->name = "Richard";
+        $user1->email = "richard@reader.com";
+        $user1->password = Hash::make('richard_1234');
+        $user1->save();
+
+        $user2 = new User;
+
+        $user2->name = "Klar";
+        $user2->email = "klar@reader.com";
+        $user2->password = Hash::make('klar_1234');
+        $user2->save();
+
+        $user3 = new User;
+
+        $user3->name = "Rena";
+        $user3->email = "Rena@reader.com";
+        $user3->password = Hash::make('rena_1234');
+        $user3->save();
+
+        $user4 = new User;
+
+        $user4->name = "Mike";
+        $user4->email = "mike@reader.com";
+        $user4->password = Hash::make('mike_1234');
+        $user4->save();
     }
 }
