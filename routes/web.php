d12daafa54841994c7dbd5f1f958dc4bf0de49fd
Richard Bernisca <taropogi_123@yahoo.com>
2021-06-30 16:16:09 +0800

    add cors routes web

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use App\Http\Controllers\CustomerItemController;
 |
 */
 
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
+header('Access-Control-Allow-Origin: *');
+
 Route::get('/', function () {
     return view('welcome');
 })->middleware('guest');
