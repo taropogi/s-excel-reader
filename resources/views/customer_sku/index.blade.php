bd19818f982fe718518b67808d4218a0c184dd1a
Richard Bernisca <taropogi_123@yahoo.com>
2021-07-02 10:42:19 +0800

    show ip

--- resources/views/customer_sku/index.blade.php
+++ resources/views/customer_sku/index.blade.php
@@ -3,6 +3,7 @@
         <x-customer-sku-sub-links />
     </x-slot>
     <div class="mt-2">
+        <h1>{{ Request::ip() }}</h1>
         <div class="max-w-9xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <livewire:page-files-sku-group />
